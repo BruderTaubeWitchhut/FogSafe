559f254e03a06dd775f74990a62c43af4f959dc7
BruderTaubeWitchhut <dyutikargv@gmail.com>
2025-05-01 21:23:50 +0530

    Add files via upload

---
+++ FogSafe.ino
@@ -0,0 +1,80 @@
+#include <LiquidCrystal.h>
+
+#define LCD_RS 12   // Register Select pin - RS of the LCD module
+#define LCD_E  11   // Enable pin - E of the LCD module
+#define LCD_D4  5   // Data4 pin - D4 of the LCD module
+#define LCD_D5  4   // Data5 pin - D5 of the LCD module
+#define LCD_D6  3   // Data6 pin - D6 of the LCD module
+#define LCD_D7  2   // Data7 pin - D7 of the LCD module
+#define LCD_COL 16  // LCD uses 16 columns
+#define LCD_ROW 2   // LCD uses 2 rows
+#define LCD_CONTRAST  6 // LCD constrast pin
+
+int Contrast = 127;
+
+LiquidCrystal lcd(LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
+int trigPin = 10;
+int echoPin = 9;
+int buzzerPin = 13;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
+  analogWrite(LCD_CONTRAST, Contrast);
+  lcd.begin(LCD_COL, LCD_ROW);
+  }
+
+float getDistanceUltrasonic() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  float duration = pulseIn(echoPin, HIGH);
+  float distance = duration * 0.0343 / 2;
+  return distance;
+}
+
+void loop() {
+ 
+  float ultrasonicDistance = getDistanceUltrasonic();
+
+  /* Displaying current distance on serial monitor */ 
+  Serial.print("Ultrasonic Distance: ");
+  Serial.println(ultrasonicDistance);
+
+  if (ultrasonicDistance < 45)
+  {
+    digitalWrite(buzzerPin, HIGH);
+    delay(500);
+    digitalWrite(buzzerPin, LOW);
+
+    lcd.setCursor(0, 0);
+    lcd.print("Alert! Obstacle");
+
+    lcd.setCursor(3, 1);
+    lcd.print(ultrasonicDistance);
+
+    lcd.setCursor(11, 1);
+    lcd.print("cm");
+
+    delay(333);
+    lcd.clear();
+  }
+  else
+  {
+    lcd.setCursor(3, 0);
+    lcd.print("Distance:");
+
+    lcd.setCursor(3, 1);
+    lcd.print(ultrasonicDistance);
+
+    lcd.setCursor(11, 1);
+    lcd.print("cm");
+
+    delay(333);
+    lcd.clear();
+  }
+}
